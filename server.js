cb503478878661f4e7e421b6d12e367a70b45b01
philippschulte <philipp.schulte@ymail.com>
2015-08-25 15:10:34 -0600

    Set up MongoDB Database

--- server.js
+++ server.js
@@ -4,11 +4,13 @@
 //Dependencies:
 //#####################################################
 var express = require('express');
+var mongoose = require('mongoose');
 var arDrone = require('ar-drone');
 
 //#####################################################
 //Config files:
 //#####################################################
+var configDB = require('./app/config/database.js');
 
 
 //#####################################################
@@ -48,14 +50,21 @@ app.use(express.static(__dirname + '/public'));
 //#####################################################
 //Starting server:
 //#####################################################
-http.listen(port, function(err) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Listening on port: ' + port);
-    }
+app.listen(port, function(err) {
+	if(err) {
+		console.log(err);
+	} else {
+		console.log('Listening on port: ' + port);
+	}
 });
 
 //#####################################################
 //Connection to database:
 //#####################################################
+mongoose.connect(configDB.uri, function(err) {
+	if (err) {
+		console.log('Connection to MongoDB failed');
+	} else {
+		console.log('Connected to MongoDB at: ', configDB.uri);
+	}
+}); 
