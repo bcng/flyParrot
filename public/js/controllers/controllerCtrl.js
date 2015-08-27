e6e6f616da4c5152666d3d307dec006a1be1994b
Brian Ng <bcn220@gmail.com>
2015-08-26 21:01:00 -0600

    add dronestream, add lib folder, add nodestream client lib on index.html

--- public/js/controllers/controllerCtrl.js
+++ public/js/controllers/controllerCtrl.js
@@ -1,68 +1,97 @@
 (function() {
     var controllerCtrl = function($scope) {
-		
-		var socket = io.connect('http://localhost:3002');
-			socket.on('connect', function() {
-				console.log('Connection successful');
-			});
+        
+    	//dronestream
+        ($scope.startArDRoneStream = function() {
+            new NodecopterStream(document.getElementById("placeholder"), {
+                port: 3001
+            });
+        }());
 
-			//drone commands
-			$scope.takeoff = function() {
-				console.log('Asking server to send takeoff command to Ar Drone');
-				socket.emit('event', {name: 'takeoff'});
-			};
+        var socket = io.connect('http://localhost:3002');
+        socket.on('connect', function() {
+            console.log('Connection successful');
+        });
 
-			$scope.land = function() {
-				console.log('Asking server to send land command to Ar Drone');
-				socket.emit('event', {name: 'land'});
-			};
+        //drone commands
+        $scope.takeoff = function() {
+            console.log('Asking server to send takeoff command to Ar Drone');
+            socket.emit('event', {
+                name: 'takeoff'
+            });
+        };
 
-			$scope.stop = function() {
-				console.log('Asking server to send stop command to Ar Drone');
-				socket.emit('event', {name: 'stop'});
-			};
+        $scope.land = function() {
+            console.log('Asking server to send land command to Ar Drone');
+            socket.emit('event', {
+                name: 'land'
+            });
+        };
 
-			//drone vertical movement
-			$scope.up = function() {
-				console.log('Asking server to send up command to Ar Drone');
-				socket.emit('event', {name: 'up'});
-			};
+        $scope.stop = function() {
+            console.log('Asking server to send stop command to Ar Drone');
+            socket.emit('event', {
+                name: 'stop'
+            });
+        };
 
-			$scope.down = function() {
-				console.log('Asking server to send down command to Ar Drone');
-				socket.emit('event', {name: 'down'});
-			};
+        //drone vertical movement
+        $scope.up = function() {
+            console.log('Asking server to send up command to Ar Drone');
+            socket.emit('event', {
+                name: 'up'
+            });
+        };
 
-			$scope.clockwise = function() {
-				console.log('Asking server to send clockwise command to Ar Drone');
-				socket.emit('event', {name: 'clockwise'});
-			};
+        $scope.down = function() {
+            console.log('Asking server to send down command to Ar Drone');
+            socket.emit('event', {
+                name: 'down'
+            });
+        };
 
-			$scope.counterClockwise = function() {
-				console.log('Asking server to send counterClockwise command to Ar Drone');
-				socket.emit('event', {name: 'counterClockwise'});
-			};
+        $scope.clockwise = function() {
+            console.log('Asking server to send clockwise command to Ar Drone');
+            socket.emit('event', {
+                name: 'clockwise'
+            });
+        };
 
-			//drone horizontal movement
-			$scope.front = function() {
-				console.log('Asking server to send front command to Ar Drone');
-				socket.emit('event', {name: 'front'});
-			};
+        $scope.counterClockwise = function() {
+            console.log('Asking server to send counterClockwise command to Ar Drone');
+            socket.emit('event', {
+                name: 'counterClockwise'
+            });
+        };
 
-			$scope.back = function() {
-				console.log('Asking server to send back command to Ar Drone');
-				socket.emit('event', {name: 'back'});
-			};
+        //drone horizontal movement
+        $scope.front = function() {
+            console.log('Asking server to send front command to Ar Drone');
+            socket.emit('event', {
+                name: 'front'
+            });
+        };
 
-			$scope.left = function() {
-				console.log('Asking server to send left command to Ar Drone');
-				socket.emit('event', {name: 'left'});
-			};
+        $scope.back = function() {
+            console.log('Asking server to send back command to Ar Drone');
+            socket.emit('event', {
+                name: 'back'
+            });
+        };
 
-			$scope.right = function() {
-				console.log('Asking server to send right command to Ar Drone');
-				socket.emit('event', {name: 'right'});
-			};
+        $scope.left = function() {
+            console.log('Asking server to send left command to Ar Drone');
+            socket.emit('event', {
+                name: 'left'
+            });
+        };
+
+        $scope.right = function() {
+            console.log('Asking server to send right command to Ar Drone');
+            socket.emit('event', {
+                name: 'right'
+            });
+        };
 
     };
 
