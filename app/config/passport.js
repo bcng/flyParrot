d988dbc9561b03665039b3ec7d476f5b86d61f4c
Brian Ng <bcn220@gmail.com>
2015-08-30 10:49:43 -0600

    add passport-local middleware to server.js file

--- app/config/passport.js
+++ app/config/passport.js
@@ -3,7 +3,7 @@
 //#####################################################
 //Dependencies:
 //#####################################################
-var LocalStrategy   = require('passport-local').Strategy;
+var LocalStrategy = require('passport-local').Strategy;
 var User = require('../models/user.js');
 
 //#####################################################
@@ -12,39 +12,40 @@ var User = require('../models/user.js');
 module.exports = function(passport) {
 
     passport.use(new LocalStrategy({
-      usernameField: 'email'
+        usernameField: 'email'
     }, function(email, password, done) {
-      //define how we match user credentials to db values
-      User.findOne({ email: email }, function(err, user) {
-        if (err) {
-          return done(err);
-        }
+        //define how we match user credentials to db values
+        User.findOne({
+            email: email
+        }, function(err, user) {
+            if (err) {
+                return done(err);
+            }
 
-        if (!user) {
-          return done(null, false, new Error('This user does not exist!'));
-        }
+            if (!user) {
+                return done(null, false, new Error('This user does not exist!'));
+            }
 
-        user.verifyPassword(password).then(function(doesMatch) {
-          if (doesMatch) {
-              delete user.password; 
-            return done(null, user);
-          }
-          else {
-            done(null, false, new Error('Please verify your password and try again!)'));
-          }
+            user.verifyPassword(password).then(function(doesMatch) {
+                if (doesMatch) {
+                    delete user.password;
+                    return done(null, user);
+                } else {
+                    done(null, false, new Error('Please verify your password and try again!)'));
+                }
+            });
         });
-      });
     }));
 
     passport.serializeUser(function(user, done) {
-      done(null, user._id);
+        done(null, user._id);
     });
 
     passport.deserializeUser(function(id, done) {
-      User.findById(id, function (err, user) {
-          delete user.password;
-        done(err, user);
-      });
+        User.findById(id, function(err, user) {
+            delete user.password;
+            done(err, user);
+        });
     });
 
 };
